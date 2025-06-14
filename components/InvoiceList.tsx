f38f59a0630fc2e0d433effb3a442a9515a892c0
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-06-14 20:01:06 +0530

    created ui for list invoices

---
+++ components/InvoiceList.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import InvoiceActions from './InvoiceActions'
+
+const InvoiceList = () => {
+  return (
+    <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead>Invoice ID</TableHead>
+                <TableHead>Customer</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className='text-right'>Actions</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow>
+                <TableCell>#1</TableCell>
+                <TableCell>Sumit Saurabh</TableCell>
+                <TableCell>$55.00</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>22/11/2024</TableCell>
+                <TableCell className='text-right'>
+                    <InvoiceActions/>
+                </TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+  )
+}
+
+export default InvoiceList
