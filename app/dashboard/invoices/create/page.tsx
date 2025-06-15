5cc5f0faa5a896aea58004e8031d2b103dbb7793
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-06-15 16:16:13 +0530

    pdf generated and send to customer

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,10 +1,27 @@
-import CreateInvoice from '@/components/CreateInvoice'
-import React from 'react'
+import prisma from "@/app/utils/db";
+import { requireUser } from "@/app/utils/hooks";
+import CreateInvoice from "@/components/CreateInvoice";
+import React from "react";
 
-const InvoiceCreationRoute = () => {
-  return (
-    <CreateInvoice />
-  )
+async function getUserData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      firstName: true,
+      lastName: true,
+      address: true,
+      email: true,
+    },
+  });
+  return data;
 }
+const InvoiceCreationRoute = async () => {
+  const session = await requireUser();
+  const data = await getUserData(session?.user?.id as string);
 
-export default InvoiceCreationRoute
+  return <CreateInvoice firstName={data?.firstName as string} lastName={data?.lastName as string} address={data?.address as string} email={data?.email as string} />;
+};
+
+export default InvoiceCreationRoute;
